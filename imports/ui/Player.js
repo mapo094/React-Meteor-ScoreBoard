69ac2824f18e9e1e9452b9bf799bc6883f9ab0c7
Mariyan <m.iliev@futuresoft.bg>
2018-03-21 13:03:46 +0200

    create new folder for style and add styles for all elements

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -6,12 +6,12 @@ import PropTypes from 'prop-types'
 export default class Player extends React.Component{
     render(){ 
         return(
-            <p key={this.props.player._id}> 
-            {this.props.player.name} has {this.props.player.score} points(s).
+            <div className="item" key={this.props.player._id}> 
+            <p>{this.props.player.name} has {this.props.player.score} points(s).</p>
             <button onClick={() => Players.update({_id: this.props.player._id},{$inc: {score: -1}})}>-1</button>
             <button onClick={() => Players.update({_id: this.props.player._id},{$inc: {score: 1}})}>+1</button>
             <button onClick={() => Players.remove({_id: this.props.player._id})}>X</button>
-        </p>
+        </div>
         )
 
     }
--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -6,7 +6,11 @@ import Player from './Player';
 export default class PlayerList extends React.Component{
     renderPlayers() {
         if(this.props.players.length === 0){
-            return <p> There are no players to display </p>
+            return (
+                <div className="item">
+                <p> There are no players to display </p>
+                </div>
+            )
         }else{
             return this.props.players.map((player) => {
                 return <Player key={player._id} player={player}/>
