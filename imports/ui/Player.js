bb89174e2f9f41656631811e854a8629d016040e
Mariyan <m.iliev@futuresoft.bg>
2018-03-21 15:26:23 +0200

    change the font-size, made + buttons rounded ,

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -8,9 +8,9 @@ export default class Player extends React.Component{
         return(
             <div className="item" key={this.props.player._id}> 
             <p>{this.props.player.name} has {this.props.player.score} points(s).</p>
-            <button onClick={() => Players.update({_id: this.props.player._id},{$inc: {score: -1}})}>-1</button>
-            <button onClick={() => Players.update({_id: this.props.player._id},{$inc: {score: 1}})}>+1</button>
-            <button onClick={() => Players.remove({_id: this.props.player._id})}>X</button>
+            <button className="button button--round" onClick={() => Players.update({_id: this.props.player._id},{$inc: {score: -1}})}>-1</button>
+            <button className="button button--round" onClick={() => Players.update({_id: this.props.player._id},{$inc: {score: 1}})}>+1</button>
+            <button className="button button--round" onClick={() => Players.remove({_id: this.props.player._id})}>X</button>
         </div>
         )
 
--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -8,7 +8,7 @@ export default class PlayerList extends React.Component{
         if(this.props.players.length === 0){
             return (
                 <div className="item">
-                <p> There are no players to display </p>
+                <p className='item__message '> There are no players to display </p>
                 </div>
             )
         }else{
