cd42c963b930bb338bb406d6cf11b64a9a75d390
Mariyan <m.iliev@futuresoft.bg>
2018-03-21 16:22:43 +0200

    style the input and the player

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <TitleBar title={this.props.title} />
+                <TitleBar title={this.props.title} subtitle="Created by Maro" />
                 <div className="wrapper">
                 <PlayerList players={this.props.players}/>
                 <AddPlayer/>
