cff766bcc730cc06b41f7f08a9d6ab07b149c52d
Mariyan <m.iliev@futuresoft.bg>
2018-04-17 18:50:47 +0300

    add last styles

--- client/main.js
+++ client/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
-import {Players} from '../imports/api/players';
+import {Players, calculatePlayerPositions} from '../imports/api/players';
 import App from '../imports/ui/App';
 
 
@@ -11,9 +11,10 @@ import App from '../imports/ui/App';
 Meteor.startup(() => {
     Tracker.autorun(() => {
         let players =  Players.find({},{sort: { score: -1}}).fetch();
+        let positionedPlayers = calculatePlayerPositions(players)
         let title = 'Score keep'
 
-        ReactDOM.render(<App title={title} players={players} />,document.getElementById('app'))
+        ReactDOM.render(<App title={title} players={positionedPlayers} />,document.getElementById('app'))
     })
 
 });
--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -5,12 +5,14 @@ import PropTypes from 'prop-types'
 
 export default class Player extends React.Component{
     render(){ 
+        let itemClassName = `item item--position-${this.props.player.rank}`;
         return(
-            <div className="item" key={this.props.player._id}> 
+            <div className={itemClassName} key={this.props.player._id}> 
                 <div className="player">
                     <div>
                         <h3 className="player__name">{this.props.player.name} </h3>
-                        <p className="player__stats">{this.props.player.score} points(s).</p>
+                        <p className="player__stats">
+                         {this.props.player.position} place - {this.props.player.score} points(s).</p>
                     </div>
                 <div className="player__action">
                     <button className="button button--round" onClick={() => Players.update({_id: this.props.player._id},{$inc: {score: -1}})}>-1</button>
